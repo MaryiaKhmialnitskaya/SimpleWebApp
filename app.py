089e8daeb7e33835c98dcb30b94c0d974bf6e69b
Maria Khmelnitskaya <Mariya.Khmyalnitskaya@bglgroup.co.uk>
2019-01-06 19:48:14 +0300

    Add home.html

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template
+)
 
-app = Flask(__name__)
+# create an instance of application
+app = Flask(__name__, template_folder="templates")
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def home():
 
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
