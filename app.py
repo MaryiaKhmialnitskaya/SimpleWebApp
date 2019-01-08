b47b18e5088017444c6d6925dbb49458db60f7cb
Maria Khmelnitskaya <Mariya.Khmyalnitskaya@bglgroup.co.uk>
2019-01-08 10:45:52 +0300

    Upadate data

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
+"""
+Main module of the server file
+"""
+
+# 3rd party moudles
 from flask import render_template
 import connexion
 
-# create an instance of the applicatoin
-app = connexion.App(__name__, specification_dir='./')
+
+# Create the application instance
+app = connexion.App(__name__, specification_dir="./")
 
 # Read the swagger.yml file to configure the endpoints
-app.add_api('swagger.yml')
+app.add_api("swagger.yml")
 
 
-@app.route('/')
+# create a URL route in our application for "/"
+@app.route("/")
 def home():
+    """
+    This function just responds to the browser URL
+    localhost:5000/
+    :return:        the rendered template "home.html"
+    """
+    return render_template("home.html")
 
-    return render_template('home.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
